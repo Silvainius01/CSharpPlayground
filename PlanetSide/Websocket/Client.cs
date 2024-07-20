dec0ce956d4b79d9535b94bbd11ee3cb00695f61
Silvainius <connor.adam@ymail.com>
2024-07-19 19:24:15 -0700

    Turns out SokcetIOSharp isnt what we need.

--- PlanetSide/Websocket/Client.cs
+++ PlanetSide/Websocket/Client.cs
@@ -1,69 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using EngineIOSharp.Common.Enum;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using SocketIOSharp.Client;
-using SocketIOSharp.Common;
-using SocketIOSharp.Server;
 
 namespace PlanetSide.WebsocketServer
 {
     public class Client
     {
-        public static void Start()
-        {
-            SocketIOClient client = new SocketIOClient(new SocketIOClientOption(EngineIOScheme.http, "localhost", 9001));
-            InitEventHandlers(client);
-
-            client.Connect();
-            Console.WriteLine("Input /exit to close connection.");
-
-            FactionTeam testTeam = new FactionTeam("test", "1", "17", Tracker.Handler);
-
-            string testStats = JsonConvert.SerializeObject(testTeam.TeamStats);
-
-
-            string line;
-            while (!(line = Console.ReadLine()).Equals("/exit"))
-            {
-                if (line.Equals("testStats"))
-                    line = testStats;
-
-                client.Emit("input", line);
-                client.Emit("input array", line, line);
-            }
-
-            client.Close();
-
-            Console.WriteLine("Press any key to continue...");
-            Console.Read();
-        }
-
-        static void InitEventHandlers(SocketIOClient client)
-        {
-            client.On(SocketIOEvent.CONNECTION, () =>
-            {
-                Console.WriteLine("Connected!");
-            });
-
-            client.On(SocketIOEvent.DISCONNECT, () =>
-            {
-                Console.WriteLine();
-                Console.WriteLine("Disconnected!");
-            });
-
-            client.On("echo", (Data) =>
-            {
-                Console.WriteLine("Echo : " + (Data[0].Type == JTokenType.Bytes ? BitConverter.ToString(Data[0].ToObject<byte[]>()) : Data[0]));
-            });
-
-            client.On("echo array", (Data) =>
-            {
-                Console.WriteLine("Echo1 : " + Data[0]);
-                Console.WriteLine("Echo2 : " + Data[1]);
-            });
-        }
     }
 }
--- PlanetSide/Websocket/Server.cs
+++ PlanetSide/Websocket/Server.cs
@@ -1,55 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
-using SocketIOSharp.Common;
-using SocketIOSharp.Server;
 
 namespace PlanetSide.WebsocketServer
 {
     public class Server
     {
-        public static void Start()
-        {
-            using (SocketIOServer server = new SocketIOServer(new SocketIOServerOption(9001)))
-            {
-                Console.WriteLine("Listening on " + server.Option.Port);
-
-                server.OnConnection((socket) =>
-                {
-                    Console.WriteLine("Client connected!");
-
-                    socket.On("input", (data) =>
-                    {
-                        foreach (JToken token in data)
-                        {
-                            Console.Write(token + " ");
-                        }
-
-                        Console.WriteLine();
-                        socket.Emit("echo", data);
-                    });
-
-                    socket.On(SocketIOEvent.DISCONNECT, () =>
-                    {
-                        Console.WriteLine("Client disconnected!");
-                    });
-
-                    socket.Emit("echo", new byte[] { 0, 1, 2, 3, 4, 5 });
-                });
-
-                server.Start();
-
-                Console.WriteLine("Input /exit to exit program.");
-                string line;
-
-                while (!(line = Console.ReadLine())?.Trim()?.ToLower()?.Equals("/exit") ?? false)
-                {
-                    server.Emit("echo", line);
-                }
-            }
-
-            Console.WriteLine("Press enter to continue...");
-            Console.Read();
-        }
     }
 }
