5357efdbcdce1c8f7ea1db2494d826a72d971d2b
Silvainius <connor.adam@ymail.com>
2025-10-26 19:26:46 -0700

    fixed build errors

--- RogueCrawler/Generation/Item/Armor/ItemArmorGenerationPresets.cs
+++ RogueCrawler/Generation/Item/Armor/ItemArmorGenerationPresets.cs
@@ -9,48 +9,7 @@ namespace RogueCrawler
 {
     
     class ItemArmorGenerationPresets
-    {
-        public static readonly Vector2Int LightWeight = new Vector2Int(MinWeaponWeight, LowWeaponWeight);
-        public static readonly Vector2Int MidWeight = new Vector2Int(LowWeaponWeight, MidWeaponWeight);
-        public static readonly Vector2Int HeavyWeight = new Vector2Int(MidWeaponWeight, MaxWeaponWeight);
-        public static readonly Vector2Int AnyWeight = new Vector2Int(MinWeaponWeight, MaxWeaponWeight);
-
-        public static ItemArmorGenerationParameters LowQualityWeaponChestItem
-        {
-            get => new ItemArmorGenerationParameters(QualityLevel.Low, DungeonGenerator.GetRandomQuality())
-            {
-                WeightRange = AnyWeight,
-                LargeWeaponProbability = GetRandomLargeProb()
-            };
-        }
-        public static ItemArmorGenerationParameters NormalQualityWeaponChestItem
-        {
-            get => new ItemArmorGenerationParameters(QualityLevel.Normal, DungeonGenerator.GetRandomQuality())
-            {
-                // QualityRange = MidQuality,
-                WeightRange = AnyWeight,
-                LargeWeaponProbability = GetRandomLargeProb()
-            };
-        }
-        public static ItemArmorGenerationParameters SuperiorQualityWeaponChestItem
-        {
-            get => new ItemArmorGenerationParameters(QualityLevel.Superior, DungeonGenerator.GetRandomQuality())
-            {
-                // QualityRange = HighQuality,
-                WeightRange = AnyWeight,
-                LargeWeaponProbability = GetRandomLargeProb()
-            };
-        }
-        public static ItemArmorGenerationParameters RandomWeaponItem
-        {
-            get => new ItemArmorGenerationParameters(DungeonGenerator.GetRandomQuality)
-            {
-                // QualityRange = AnyQuality,
-                WeightRange = AnyWeight,
-                LargeWeaponProbability = MidLargeRate
-            };
-        }
-        
+    {        
         public static ItemArmorGenerationParameters GetParamsForChest(int level, QualityLevel quality)
         {
             return new ItemArmorGenerationParameters(quality, DungeonGenerator.GetRandomQuality())
